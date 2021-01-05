56f86d706a3ee0a53d37eda182be251aea984265
Taka <taka717@gmail.com>
2021-01-05 21:45:17 +0900

    fix(webview): update webview logic

--- webview.js
+++ webview.js
@@ -1,7 +1,13 @@
 "use strict";
 
-module.exports = Franz => {
-  function getMessages() {
+var _electron = require("electron");
+
+const {
+  BrowserWindow
+} = _electron.remote;
+
+module.exports = (Franz, settings) => {
+  const getMessages = function getMessages() {
     let directCount   = 0;
     let indirectCount = 0;
 
@@ -14,4 +20,34 @@ module.exports = Franz => {
   };
 
   Franz.loop(getMessages);
+
+  if (settings.franzVersion) {
+    document.addEventListener('click', event => {
+      const link = event.target.closest('a[href^="http"]');
+      const button = event.target.closest('button[title^="http"]');
+
+      if (link || button) {
+        const url = link ? link.getAttribute('href') : button.getAttribute('title');
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (url.includes('views/imgpsh_fullsize_anim')) {
+          let win = new BrowserWindow({
+            width: 800,
+            height: window.innerHeight,
+            minWidth: 600,
+            webPreferences: {
+              partition: `persist:service-${settings.id}`
+            }
+          });
+          win.loadURL(url);
+          win.on('closed', () => {
+            win = null;
+          });
+        } else {
+          window.open(url);
+        }
+      }
+    }, true);
+  }
 };
